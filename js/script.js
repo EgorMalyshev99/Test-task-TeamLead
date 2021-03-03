c06cfcb5dea8d7f46c6f87a89ced46111d71c763
Egor Malyshev <egor.mlshv99@mail.ru>
2021-03-04 02:11:42 +0300

    +slider

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function clock() {
-  var date = new Date(),
+  let date = new Date(),
     hours = date.getHours() < 10 ? "0" + date.getHours() : date.getHours(),
     minutes =
       date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes(),
@@ -11,16 +11,49 @@ function clock() {
 
 setInterval(clock, 1000);
 
-var slider = document.querySelectorAll("#slider .slide");
-var currentslide = 0;
+const slider = document.querySelectorAll("#slider .slide");
+const dots = document.querySelectorAll("#dots .dot");
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+let currentSlide = 0;
+let currentDot = 0;
 
-var slideInterval = setInterval(
-  nextSlide,
-  5000
-); /* Интервал между картинками */
+// const slideInterval = setInterval(
+//   prevSlide,
+//   5000
+// );
 
 function nextSlide() {
-  slider[currentslide].className = "slide";
-  currentslide = (currentslide + 1) % slider.length;
-  slider[currentslide].className = "slide demonstration";
+  slider[currentSlide].className = "slide";
+  dots[currentDot].className = "dot";
+  currentSlide = (currentSlide + 1) % slider.length;
+  currentDot = (currentDot + 1) % dots.length;
+  slider[currentSlide].className = "slide demonstration";
+  dots[currentDot].className = "dot active-dot";
 }
+
+function prevSlide() {
+  slider[currentSlide].className = "slide";
+  dots[currentDot].className = "dot";
+
+  if (currentSlide === 0) {
+    currentSlide = slider.length - 1;
+    currentDot = dots.length - 1;
+  } else {
+    currentSlide = currentSlide - 1;
+    currentDot = currentDot - 1;
+  }
+
+  slider[currentSlide].className = "slide demonstration";
+  dots[currentDot].className = "dot active-dot";
+}
+
+nextBtn.addEventListener("click", (evt) => {
+  console.log("next");
+  nextSlide();
+});
+
+prevBtn.addEventListener("click", (evt) => {
+  console.log("prev");
+  prevSlide();
+});
